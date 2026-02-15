ac28e1f6ca7ed330551e71054fc733be00e9368f
Manasvi <manasvi.tiwari@proton.me>
2026-02-15 09:07:24 +0530

    register designer

---
+++ DevBoard/Register.aspx.designer.cs
@@ -0,0 +1,15 @@
+namespace DevBoard
+{
+    public partial class Register
+    {
+        protected global::System.Web.UI.WebControls.TextBox EmailTextBox;
+        protected global::System.Web.UI.WebControls.TextBox PasswordTextBox;
+        protected global::System.Web.UI.WebControls.TextBox ConfirmPasswordTextBox;
+        protected global::System.Web.UI.WebControls.DropDownList RoleDropDown;
+        protected global::System.Web.UI.WebControls.Label ErrorLabel;
+        protected global::System.Web.UI.WebControls.Button RegisterButton;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator EmailRequired;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator PasswordRequired;
+        protected global::System.Web.UI.WebControls.CompareValidator PasswordCompare;
+    }
+}
