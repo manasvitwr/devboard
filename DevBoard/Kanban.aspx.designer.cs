852051a7219db97493b0085bd5a0b2b6d0d589fe
Manasvi <manasvi.tiwari@proton.me>
2026-02-15 09:07:25 +0530

    kanban designer

---
+++ DevBoard/Kanban.aspx.designer.cs
@@ -0,0 +1,14 @@
+namespace DevBoard
+{
+    public partial class Kanban
+    {
+        protected global::System.Web.UI.WebControls.DropDownList ProjectDropDown;
+        protected global::System.Web.UI.UpdatePanel KanbanUpdatePanel;
+        protected global::System.Web.UI.WebControls.Panel TodoPanel;
+        protected global::System.Web.UI.WebControls.Panel InProgressPanel;
+        protected global::System.Web.UI.WebControls.Panel DonePanel;
+        protected global::System.Web.UI.WebControls.Repeater TodoRepeater;
+        protected global::System.Web.UI.WebControls.Repeater InProgressRepeater;
+        protected global::System.Web.UI.WebControls.Repeater DoneRepeater;
+    }
+}
