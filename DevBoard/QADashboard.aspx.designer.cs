6c2e313fa910b8e3512e8ada751bbd1b51c01bc4
Manasvi <manasvi.tiwari@proton.me>
2026-02-15 09:07:29 +0530

    qa designer

---
+++ DevBoard/QADashboard.aspx.designer.cs
@@ -0,0 +1,13 @@
+namespace DevBoard
+{
+    public partial class QADashboard
+    {
+        protected global::System.Web.UI.WebControls.DropDownList ProjectDropDown;
+        protected global::System.Web.UI.WebControls.Label TotalTicketsLabel;
+        protected global::System.Web.UI.WebControls.Label QADebtLabel;
+        protected global::System.Web.UI.WebControls.Label FlakyLabel;
+        protected global::System.Web.UI.WebControls.Label MissingTestsLabel;
+        protected global::System.Web.UI.WebControls.GridView PainScoreGridView;
+        protected global::System.Web.UI.WebControls.HiddenField ChartDataHidden;
+    }
+}
