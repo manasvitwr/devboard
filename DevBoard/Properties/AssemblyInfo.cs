b1c0e4e6bf92d8434ac118090c98bfe06ab25ee5
Manasvi <manasvi.tiwari@proton.me>
2026-02-15 09:07:47 +0530

    assembly info

---
+++ DevBoard/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevBoard")]
+[assembly: AssemblyDescription("Project Management and QA Tracking Tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevBoard")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b9cad0e-1f20-4192-bff0-e1e043cea4db")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
