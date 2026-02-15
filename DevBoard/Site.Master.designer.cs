765f97a6c477409f030a5ea3c78cb4ba60a91c87
Manasvi <manasvi.tiwari@proton.me>
2026-02-15 09:07:18 +0530

    layout designer

---
+++ DevBoard/Site.Master.designer.cs
@@ -0,0 +1,9 @@
+namespace DevBoard
+{
+    public partial class SiteMaster
+    {
+        protected global::System.Web.UI.WebControls.LinkButton LogoutButton;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+    }
+}
