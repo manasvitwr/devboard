e2fbb0d2aee9783d9ab316b900ed2cac545b3dfa
Manasvi <manasvi.tiwari@proton.me>
2026-02-15 09:07:28 +0530

    projects designer

---
+++ DevBoard/Projects.aspx.designer.cs
@@ -0,0 +1,19 @@
+namespace DevBoard
+{
+    public partial class Projects
+    {
+        protected global::System.Web.UI.WebControls.Button ShowCreateButton;
+        protected global::System.Web.UI.WebControls.Label MessageLabel;
+        protected global::System.Web.UI.WebControls.Panel CreatePanel;
+        protected global::System.Web.UI.WebControls.HiddenField ProjectIdHidden;
+        protected global::System.Web.UI.WebControls.TextBox NameTextBox;
+        protected global::System.Web.UI.WebControls.TextBox DescriptionTextBox;
+        protected global::System.Web.UI.WebControls.TextBox RepoUrlTextBox;
+        protected global::System.Web.UI.WebControls.TextBox ConfigPathTextBox;
+        protected global::System.Web.UI.WebControls.Button SaveButton;
+        protected global::System.Web.UI.WebControls.Button CancelButton;
+        protected global::System.Web.UI.WebControls.Repeater ProjectsRepeater;
+        protected global::System.Web.UI.WebControls.Panel NoProjectsPanel;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator NameRequired;
+    }
+}
