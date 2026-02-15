65ab452a837bbe8076c46e98521110716ae9baf4
Manasvi <manasvi.tiwari@proton.me>
2026-02-15 09:07:26 +0530

    modules designer

---
+++ DevBoard/Modules.aspx.designer.cs
@@ -0,0 +1,8 @@
+namespace DevBoard
+{
+    public partial class Modules
+    {
+        protected global::System.Web.UI.WebControls.Label ProjectNameLabel;
+        protected global::System.Web.UI.WebControls.GridView ModulesGridView;
+    }
+}
