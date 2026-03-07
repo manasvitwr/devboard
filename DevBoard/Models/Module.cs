e5d1d0abda83f8a315cfcd39293e6647d27d71a4
Manasvi <manasvi.tiwari@proton.me>
2026-03-08 02:17:02 +0530

    upd module model to make path optional for nested categories logic

--- DevBoard/Models/Module.cs
+++ DevBoard/Models/Module.cs
@@ -8,21 +8,31 @@ namespace DevBoard.Models
         public Module()
         {
             Tickets = new List<Ticket>();
+            Categories = new List<Category>();
         }
 
         public int Id { get; set; }
 
         public int ProjectId { get; set; }
 
+        [StringLength(255)]
+        public string ExtId { get; set; }
+
+        [StringLength(1024)]
+        public string Description { get; set; }
+
+        public bool IsCritical { get; set; }
+
+
         [Required]
         [StringLength(200)]
         public string Name { get; set; }
 
-        [Required]
         [StringLength(500)]
         public string Path { get; set; }
 
         public virtual Project Project { get; set; }
         public virtual ICollection<Ticket> Tickets { get; set; }
+        public virtual ICollection<Category> Categories { get; set; }
     }
 }
